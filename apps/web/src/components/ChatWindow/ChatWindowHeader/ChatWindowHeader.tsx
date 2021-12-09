1dd948d0411becdb2d03467844fadecfea17d9ff
Lee <leewhite128@gmail.com>
2021-12-09 12:34:28 -0800

    chore: chatWindowHeader

--- apps/web/src/components/ChatWindow/ChatWindowHeader/ChatWindowHeader.tsx
+++ apps/web/src/components/ChatWindow/ChatWindowHeader/ChatWindowHeader.tsx
@@ -1,43 +1,33 @@
 import React from 'react';
-import { makeStyles, createStyles } from '@material-ui/core/styles';
-import CloseIcon from '../../../icons/CloseIcon';
+
+import { Heading } from '@twilio-paste/core/heading';
+import { Button } from '@twilio-paste/core/button';
+import { Box } from '@twilio-paste/core/box';
+
+import { CloseIcon } from '@twilio-paste/icons/esm/CloseIcon';
 
 import useChatContext from '../../../hooks/useChatContext/useChatContext';
 
-const useStyles = makeStyles(() =>
-  createStyles({
-    container: {
-      height: '56px',
-      background: '#F4F4F6',
-      borderBottom: '1px solid #E4E7E9',
-      display: 'flex',
-      justifyContent: 'space-between',
-      alignItems: 'center',
-      padding: '0 1em',
-    },
-    text: {
-      fontWeight: 'bold',
-    },
-    closeChatWindow: {
-      cursor: 'pointer',
-      display: 'flex',
-      background: 'transparent',
-      border: '0',
-      padding: '0.4em',
-    },
-  })
-);
-
 export default function ChatWindowHeader() {
-  const classes = useStyles();
   const { setIsChatWindowOpen } = useChatContext();
 
   return (
-    <div className={classes.container}>
-      <div className={classes.text}>Chat</div>
-      <button className={classes.closeChatWindow} onClick={() => setIsChatWindowOpen(false)}>
-        <CloseIcon />
-      </button>
-    </div>
+    <Box
+      display="flex"
+      justifyContent="space-between"
+      alignItems="center"
+      padding="space50"
+      borderBottomColor="colorBorderLight"
+      backgroundColor="colorBackground"
+      borderBottomStyle="solid"
+      borderBottomWidth="borderWidth10"
+    >
+      <Heading as="h2" variant="heading50" marginBottom="space0">
+        Chat
+      </Heading>
+      <Button variant="secondary_icon" onClick={() => setIsChatWindowOpen(false)} size="icon_small">
+        <CloseIcon decorative={false} title="Close chat" />
+      </Button>
+    </Box>
   );
 }
